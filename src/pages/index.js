73e5b218e352202aabee5fe2c41396b0cf5a0226
梅林的法杖 <530264127@qq.com>
2021-09-28 23:24:44 +0800

    runtime data fetch

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,27 @@
-import * as React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 import Layout from '../components/Layout/Layout'
 
 const IndexPage = () => {
+  const [list, setList] = useState([])
+  useEffect(() => {
+    axios.get("https://cnodejs.org/api/v1/topics").then(res => {
+      if (res.data.success) {
+        setList(res.data.data ?? [])
+      }
+    })
+  }, [])
   return (
     <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <ul>
+        {list.map((item, index) => {
+          return (
+            <li key={index}>
+              {item.title}
+            </li>
+          )
+        })}
+      </ul>
     </Layout>
   )
 }
